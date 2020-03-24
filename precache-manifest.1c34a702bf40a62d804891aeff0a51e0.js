cd4b1315dd034d6b475c69acc491f851c82964bd
Timothy Lai <timothy.lai@gmail.com>
2020-03-23 17:42:05 -0700

    Updates

---
+++ precache-manifest.1c34a702bf40a62d804891aeff0a51e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f12d69889ed991d7746847c3f1585808",
+    "url": "/swagger-ui-demo/index.html"
+  },
+  {
+    "revision": "3b19c5da2b0ae5fe76bb",
+    "url": "/swagger-ui-demo/static/css/2.677ef6f3.chunk.css"
+  },
+  {
+    "revision": "e5e7aeed6ee103359fd5",
+    "url": "/swagger-ui-demo/static/css/main.36e7dac0.chunk.css"
+  },
+  {
+    "revision": "3b19c5da2b0ae5fe76bb",
+    "url": "/swagger-ui-demo/static/js/2.21ae2aab.chunk.js"
+  },
+  {
+    "revision": "7bf5c54fb7e8b4f2cd505c24df446e0d",
+    "url": "/swagger-ui-demo/static/js/2.21ae2aab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5e7aeed6ee103359fd5",
+    "url": "/swagger-ui-demo/static/js/main.cd86c993.chunk.js"
+  },
+  {
+    "revision": "5796c535d2e245fa69e9",
+    "url": "/swagger-ui-demo/static/js/runtime-main.fb04ee9a.js"
+  },
+  {
+    "revision": "225610e724f43789256e56fd8bc7ef5b",
+    "url": "/swagger-ui-demo/static/media/wow-alliance-logo.225610e7.png"
+  }
+]);
